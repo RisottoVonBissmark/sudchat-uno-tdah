d2e0b73f07b0a297f25ff0e742911f25e143e6b3
kamai7 <anaellecarre35@gmail.com>
2025-12-05 07:24:02 +0100

    market button

--- frontend/src/playground/MarketButton.jsx
+++ frontend/src/playground/MarketButton.jsx
@@ -1,7 +1,6 @@
 
 
 export function MarketButton({ onClick }) {
-  console.log(onClick);
   return (
     <button id="market-button" onClick={(e) => onClick()} ><img src="/public/svg/market.svg"/></button>
   );
