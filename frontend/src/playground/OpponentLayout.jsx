1f33d808e66486ead4146b016c36bfda31149732
Rag404 <tt.favnec.yt@gmail.com>
2025-12-05 06:33:43 +0100

    pile

--- frontend/src/playground/OpponentLayout.jsx
+++ frontend/src/playground/OpponentLayout.jsx
@@ -2,9 +2,9 @@ import { Opponent } from "./Opponent";
 
 function position(index, opponentCount) {
   const angle = (index / (opponentCount-1)) * Math.PI;
-  const radius = 150;
-  const x = radius * -Math.cos(angle);
-  const y = radius * -Math.sin(angle);
+  const radius = 10;
+  const x = 14 * -Math.cos(angle);
+  const y = 10 * -Math.sin(angle);
   return [x, y];
 }
 
--- frontend/src/playground/Playground.jsx
+++ frontend/src/playground/Playground.jsx
@@ -1,15 +1,18 @@
 import { UnoButton } from "./UnoButton";
 import { CounterButton } from "./CounterButton";
 import { OpponentLayout } from "./OpponentLayout";
+import { Pile } from "./cards/Pile";
 import { PlayerHand } from "./PlayerHand";
 import './playground.css';
 
-export function Playground({ hand, opponents }) {
+
+export function Playground({ gameId, hand, opponents }) {
   return (
     <div className="playground">
       <UnoButton />
       <CounterButton />
       <OpponentLayout opponents={opponents} />
+      <Pile topCard={{ type: "normal" }} />
       <PlayerHand cards={hand} />
     </div>
   )
