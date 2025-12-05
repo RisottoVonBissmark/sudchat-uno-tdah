ca322a83a08e223a276b6a42d815a5bff94c632f
kamai7 <anaellecarre35@gmail.com>
2025-12-05 05:10:45 +0100

    boutique

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {CardContainer} from './affichageCarteJoueurAnaelle/CarteJoueur'
+import {Boutique} from './affichageCarteJoueurAnaelle/Boutique'
 
 function App() {
   const listCardsTemp = [
@@ -20,11 +21,104 @@ function App() {
             url: "public/cartes/0red.svg"
         }
     ];
-  const [count, setCount] = useState(0);
+
+  const productsTemp = [
+        {
+            key : crypto.randomUUID(),
+            name: "skin chat",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "bibite",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "jsp",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "skin chat",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "bibite",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "jsp",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "skin chat",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "bibite",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "jsp",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "skin chat",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "bibite",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "jsp",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "skin chat",
+            url: "public/cartes/0red.svg",
+            price: 934
+        },
+        {
+            key : crypto.randomUUID(),
+            name: "bibite",
+            url: "public/cartes/0red.svg",
+            price: 934
+        }
+    ];
+  const [boutique, setBoutique] = useState(true);
+
+  function closeBoutique(){
+    setBoutique(false);
+  }
   
   return (
     <>
-      <CardContainer width='200px' size="10" cards={listCardsTemp}/>
+    {boutique && <Boutique products={productsTemp} coins="3000" onClose={closeBoutique}/>}
+      
+      {/* <CardContainer width='200px' size="10" cards={listCardsTemp}/>*/}
     </>
   )
 }
